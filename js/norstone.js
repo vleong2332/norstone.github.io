d18af8f7e03b136024026bf59680a198e75ff589
werckerbot <pleasemailus@wercker.com>
2016-06-08 04:36:34 +0000

    [ci skip] deploy from avolkman

--- js/norstone.js
+++ js/norstone.js
@@ -1,9 +1,12 @@
 $(document).foundation();
+
 $(function() {
+  // Change page on dropdown click in gallery.
   $('.gallery-drop select').change(function() {
     window.location = $(this).val();
   });
 
+  // Add Pinterest social share icon.
   if ($('.social-share-container').length > 0) {
     var pinterest = {
       url: document.URL,
@@ -13,4 +16,22 @@ $(function() {
     var href = '//pinterest.com/pin/create/button/?' + $.param(pinterest);
     $('.social-share-container').prepend('<a class="social-share-pinterest" target="_blank" href="' + href + '">Pinterest</a>');
   }
+
+  // Smooth scrolling.
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    // Don't break tabs.
+    if ($(this).parents('.tabs').length) {
+      return true;
+    }
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
