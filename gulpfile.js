86b5d0ff81d95f82957a64a976615ae313e9fd96
Albert Volkman <albert.volkman@gmail.com>
2016-11-17 18:51:41 -0500

    [#48] Change base docker image

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,40 @@
+var child = require('child_process');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var filelog = require('gulp-filelog');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
-var child = require('child_process');
-var es = require('event-stream');
+var sequence = require('run-sequence');
 
-gulp.task('default', ['sass', 'js', 'jekyll:serve', 'sass:watch']);
-gulp.task('build', ['sass', 'js', 'jekyll']);
+gulp.task('default', function(done) {
+  sequence('sass', 'js', 'jekyll:serve', 'sass:watch', done);
+});
+
+gulp.task('build', function(done) {
+  sequence('sass', 'js', 'jekyll', done);
+});
 
 gulp.task('sass', function() {
-  return es.merge(
-    gulp.src('./_css/norstone.scss')
-      .pipe(sass({
-        includePaths: [
-          './_css',
-          './bower_components/foundation-sites/scss',
-          './bower_components/motion-ui/src',
-          './bower_components/owl.carousel/src/scss'
-        ],
-        outputStyle: 'compressed'
-      }).on('error', sass.logError)),
-      gulp.src('./_css/landing.css')
-    )
-    .pipe(gulp.dest('./css'));
+  // Move static CSS files.
+  gulp.src('./_css/landing.css')
+    .pipe(gulp.dest('./css'))
+    .pipe(filelog());
+
+  // Compile SASS.
+  return gulp.src('./_css/norstone.scss')
+    .pipe(sass({
+      includePaths: [
+        './_css',
+        './bower_components/foundation-sites/scss',
+        './bower_components/motion-ui/src',
+        './bower_components/owl.carousel/src/scss'
+      ],
+      outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(filelog());
 });
 
 gulp.task('sass:watch', function() {
@@ -31,20 +42,24 @@ gulp.task('sass:watch', function() {
 });
 
 gulp.task('js', function() {
-  return es.merge(
-    gulp.src([
-      './_js/jquery.js',
-      './_js/what-input.js',
-      './_js/foundation.js',
-      './_js/owl.carousel.min.js',
-      './_js/owl.carousel2.thumbs.js',
-      './_js/norstone.js'
-      ])
-      .pipe(concat('norstone.js'))
-      .pipe(uglify()),
-    gulp.src('./_js/landing.js')
-  )
-  .pipe(gulp.dest('js'));
+  // Move static JS files.
+  gulp.src('./_js/landing.js')
+    .pipe(gulp.dest('js'))
+    .pipe(filelog());
+
+  // Compile JS.
+  return gulp.src([
+    './_js/jquery.js',
+    './_js/what-input.js',
+    './_js/foundation.js',
+    './_js/owl.carousel.min.js',
+    './_js/owl.carousel2.thumbs.js',
+    './_js/norstone.js'
+    ])
+    .pipe(concat('norstone.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'))
+    .pipe(filelog());
 });
 
 gulp.task('jekyll', function() {
