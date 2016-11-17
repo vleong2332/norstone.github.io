237529cefd63354b80c8b5842972e3ff2cc5842c
Albert Volkman <albert.volkman@gmail.com>
2016-11-17 15:19:02 -0500

    [#48] Use gulp to do Jekyll build

--- gulpfile.js
+++ gulpfile.js
@@ -2,47 +2,55 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var child = require('child_process');
 var gutil = require('gulp-util');
+var es = require('event-stream');
 
 gulp.task('default', ['sass', 'jekyll:serve', 'sass:watch']);
 gulp.task('build', ['sass', 'jekyll']);
 
-gulp.task('sass', function () {
-  return gulp.src('./_css/norstone.scss')
-    .pipe(sass({
-      includePaths: [
-        './_css',
-        './bower_components/foundation-sites/scss',
-        './bower_components/motion-ui/src',
-        './bower_components/owl.carousel/src/scss'
-      ]
-    }).on('error', sass.logError))
+gulp.task('sass', function() {
+  return es.merge(
+    gulp.src('./_css/norstone.scss')
+      .pipe(sass({
+        includePaths: [
+          './_css',
+          './bower_components/foundation-sites/scss',
+          './bower_components/motion-ui/src',
+          './bower_components/owl.carousel/src/scss'
+        ]
+      }).on('error', sass.logError)),
+      gulp.src('./_css/landing.css')
+    )
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', function() {
   gulp.watch('./_css/**/*.scss', ['sass']);
 });
 
-gulp.task('jekyll', () => {
+gulp.task('jekyll', function() {
   const jekyll = child.spawn('jekyll', ['build']);
 
-  const jekyllLogger = (buffer) => {
+  const jekyllLogger = function(buffer) {
     buffer.toString()
       .split(/\n/)
-      .forEach((message) => gutil.log('Jekyll: ' + message));
+      .forEach(function(message) {
+        gutil.log('Jekyll: ' + message);
+      });
   };
 
   jekyll.stdout.on('data', jekyllLogger);
   jekyll.stderr.on('data', jekyllLogger);
 });
 
-gulp.task('jekyll:serve', () => {
+gulp.task('jekyll:serve', function() {
   const jekyll = child.spawn('jekyll', ['serve']);
 
-  const jekyllLogger = (buffer) => {
+  const jekyllLogger = function(buffer) {
     buffer.toString()
       .split(/\n/)
-      .forEach((message) => gutil.log('Jekyll: ' + message));
+      .forEach(function(message) {
+        gutil.log('Jekyll: ' + message);
+      });
   };
 
   jekyll.stdout.on('data', jekyllLogger);
