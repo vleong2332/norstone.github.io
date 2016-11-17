bc4d21fd7f2fa3a1aaa7257f0941f0b8a447d5da
Albert Volkman <albert.volkman@gmail.com>
2016-11-17 12:56:05 -0500

    [#48] Gulp task: compile SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var child = require('child_process');
 var gutil = require('gulp-util');
+
+gulp.task('sass', function () {
+  return gulp.src('./_css/norstone.scss')
+    .pipe(sass({
+      includePaths: [
+        './_css',
+        './bower_components/foundation-sites/scss',
+        './bower_components/motion-ui/src',
+        './bower_components/owl.carousel/src/scss'
+      ]
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./_css/**/*.scss', ['sass']);
+});
 
 gulp.task('jekyll', () => {
   const jekyll = child.spawn('jekyll', ['serve']);
