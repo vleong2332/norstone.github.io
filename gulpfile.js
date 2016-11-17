4ed8534c98107c3cda6169e0f379c1a710e612be
Albert Volkman <albert.volkman@gmail.com>
2016-11-17 15:07:14 -0500

    [#48] Gulp task: Jekyll build

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var sass = require('gulp-sass');
 var child = require('child_process');
 var gutil = require('gulp-util');
 
-gulp.task('default', ['sass', 'jekyll', 'sass:watch']);
+gulp.task('default', ['sass', 'jekyll:serve', 'sass:watch']);
+gulp.task('build', ['sass', 'jekyll']);
 
 gulp.task('sass', function () {
   return gulp.src('./_css/norstone.scss')
@@ -23,6 +24,19 @@ gulp.task('sass:watch', function () {
 });
 
 gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['build']);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('jekyll:serve', () => {
   const jekyll = child.spawn('jekyll', ['serve']);
 
   const jekyllLogger = (buffer) => {
